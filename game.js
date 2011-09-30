2ad93e38704194aabfabf76dcfe2f15a290dbeec
Mike <github@ravuya.com>
2011-09-29 22:40:12 -0600

    Loading image

--- game.js
+++ game.js
@@ -4,12 +4,22 @@ var keyboard;
 
 var interval = 1000/20; // 20 fps
 
+var imagePaths = [ 'assets/block.png' ];
+var images = new Array();
+
 /// game entry point
 main = function() {
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");
 	
 	keyboard = new Keyboard();
+	
+	// load images
+	for(path in imagePaths) {
+		streamedImage = new StreamedImage();
+		streamedImage.image.src = path;
+		images.push(streamedImage);
+	}
 	
 	setInterval(step, interval);
 }
